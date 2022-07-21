65bb13731ed5cc28a4a86c675cffa851659fb333
amanda <amanlsmoura@gmail.com>
2022-07-21 17:27:05 -0300

    barbosa-amanda-moura

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let numPar = +prompt('Digite um número par para começarmos!')
+
+console.log(numPar%2)
+
+//números pares não possuem resto, sendo assim, sempre irá ficar zerado
+
+//Caso inseridos números ímpares, sempre irá restar um)
+
